6713ff378d2d4b727d8448f91b3b7adf42eb8525
darrenlimkh <darrenkhlim@live.com.sg>
2022-10-22 19:32:40 +0800

    darren update

---
+++ README.md
@@ -0,0 +1,5 @@
+# BT5110 Starapp Project
+## Getting Started
+1. Activate virtual environment: ```env\Scripts\activate```
+2. Run application: ```python app.py```
+3. Local host: ```http://127.0.0.1:1234```
--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from flask import Flask, redirect, render_template, request
 import numpy as np
 import random
 import json
-import os
 
 app = Flask(__name__)
+CONFIG = json.load(open('./config/config.json')) # for future configs
 
+# dummy data: actual data can store in temporarily store in JSON moving forward
 viewership = {
-	'data': [123456,234567,345678,456789,567890,678901]
+	'data': [123456,234567,345678,456789,567890,678901],
+	'label': 'Viewership'
 }
-	
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+	if request.method == 'POST':
+		# read user's dropdown input
+		if request.form.get("submit-button") == "Submit":
+			print(request.form["period"], request.form["genre"])
+
 	return render_template('home.html', data=viewership)
 
 if __name__ == "__main__":
